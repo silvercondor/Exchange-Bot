aec1190d93f8d9d91175f73b811a5a54efdeb80c
Simon Tan Yu Jing <simon.tan.yu.jing@gmail.com>
2018-02-11 15:50:42 +0800

    added unknown handler and reply to quotes.
    additional things to do left
    # Move functions to function.py, leave the messagehandler and add handler in this file under a main function
    # Add description to commands
    # Send hyperlink instead of just name, use name as text in a href
    # Add volume
    # and the actual acccumulation part lol

--- AccumulationBot.py
+++ AccumulationBot.py
@@ -3,6 +3,7 @@ import telegram
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import functions
+from random import randint
 
 # Initiate logging module
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
@@ -22,7 +23,7 @@ coin_to_exchanges = dict()
 # Initialize a dict of dictionaries for database
 database = dict()
 
-# Function to update database
+# Command to update database
 def updateDB(bot, update):
 	bot.send_message(chat_id=update.message.chat_id, text='Updating database...')
 	functions.updateDB(database)
@@ -31,11 +32,11 @@ updateDB_handler = CommandHandler('updateDB', updateDB)
 dispatcher.add_handler(updateDB_handler)
 
 
-# Creating an exchange command to find all exchanges that trades this coin
+# Command to find all exchanges that trades this coin
 def exchange(bot, update, args):
 	print()
 	if(len(args) != 1):
-		bot.send_message(chat_id=update.message.chat_id, text='Too few / many arguments! Please enter only 1 ticker.')
+		bot.send_message(chat_id=update.message.chat_id, text='Too few / many arguments! Please enter only 1 ticker.', reply_to_message_id=update.message.message_id)
 	else:
 		# Getting the id equivalent (full name delimited with '-') of the target coin
 		coin = args[0].upper()
@@ -47,35 +48,47 @@ def exchange(bot, update, args):
 
 				# list_of_exchanges is a list of concatenated exchanges into strings with a maximum length of 4096 characters, see comments in the concatExchange Function as to why this isn't necessary for now.
 				# list_of_exchanges = []
+				
 				list_of_exchanges = functions.concatExchanges(exchanges)
-				bot.send_message(chat_id=update.message.chat_id, text=list_of_exchanges)
+				bot.send_message(chat_id=update.message.chat_id, text=list_of_exchanges, reply_to_message_id=update.message.message_id)
 				#for i in list_of_exchanges:
-					#bot.send_message(chat_id=update.message.chat_id, text=i)
+					#bot.send_message(chat_id=update.message.chat_id, text=i, reply_to_message_id=update.message.message_id)
 		else:
 			print(args[0] + " not found!")
-			bot.send_message(chat_id=update.message.chat_id, text=args[0] + " cannot be found in DB, please run the updateDB command or check that you've entered a valid ticker.")
+			bot.send_message(chat_id=update.message.chat_id, text=args[0] + " cannot be found in DB, please run the updateDB command or check that you've entered a valid ticker.", reply_to_message_id=update.message.message_id)
 
 exchange_handler = CommandHandler('exchange', exchange, pass_args=True)
 dispatcher.add_handler(exchange_handler)
 
-# Creating an exchange command to find all exchanges that trades this coin
+# Command to update list of exchanges that trades this coin
 def updateExchange(bot, update, args):
 	print()
 	if(len(args) != 1):
-		bot.send_message(chat_id=update.message.chat_id, text='Too few / many arguments! Please enter only 1 ticker.')
+		bot.send_message(chat_id=update.message.chat_id, text='Too few / many arguments! Please enter only 1 ticker.', reply_to_message_id=update.message.message_id)
 	else:
 		# Getting the id equivalent (full name delimited with '-') of the target coin
 		coin = args[0].upper()
 		if(coin in database):
 			id = database[coin].get('id')
 			functions.updateExchange(id, coin_to_exchanges)
-			bot.send_message(chat_id=update.message.chat_id, text="Exchanges for " + args[0] + " is updated!")
+			bot.send_message(chat_id=update.message.chat_id, text="Exchanges for " + args[0] + " is updated!", reply_to_message_id=update.message.message_id)
 		else:
 			print(args[0] + " not found!")
-			bot.send_message(chat_id=update.message.chat_id, text=args[0] + " cannot be found in DB, please run the updateDB command or check that you've entered a valid ticker.")
+			bot.send_message(chat_id=update.message.chat_id, text=args[0] + " cannot be found in DB, please run the updateDB command or check that you've entered a valid ticker.", reply_to_message_id=update.message.message_id)
 
 updateExchange_handler = CommandHandler('updateExchange', updateExchange, pass_args=True)
 dispatcher.add_handler(updateExchange_handler)
+
+
+# Command to handle unregistered commands
+def unknown(bot, update):
+	replies = ["Stop trolling my bot Brian!", "Invalid command...", "Please try again later even though it'll probably still not work lol.", "404 Error: Command not found!"]
+	bot.send_message(chat_id=update.message.chat_id, text=replies[randint(0,3)], reply_to_message_id=update.message.message_id)
+
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
+
+
 
 # Start the bot
 print('starting bot!')
@@ -83,8 +96,9 @@ updater.start_polling(clean=True)
 
 
 # Additional Features
-# Quote the message and reply to it.
-# Send hyperlink instead of just name, use name as text in a href
-# Command to handle all unregistered command
+
+# Move functions to function.py, leave the messagehandler and add handler in this file under a main function
 # Add description to commands
+# Send hyperlink instead of just name, use name as text in a href
 # Add volume
+# and the actual acccumulation part lol
