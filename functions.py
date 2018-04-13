236330c9791c668f30c5fd6bbaa759047b97c8da
Simon Tan Yu Jing <simon.tan.yu.jing@gmail.com>
2018-04-13 10:37:59 +0800

    Forgot to uncomment some lines during the previous test.

--- functions.py
+++ functions.py
@@ -83,7 +83,6 @@ def updateCoin(coin, coin_to_exchanges):
 		while(i < len(rows)):
 
 			exchange = (list(list(rows[i])[3].children)[1]).get_text()
-			print(exchange)
 
 			# Remove $ and commas for volume and convert to int
 			vol = int(re.sub("[^\d\.]", "", (list(list(rows[i])[7].children)[1]).get_text()))
@@ -110,7 +109,7 @@ def updateCoin(coin, coin_to_exchanges):
 
 		# If you're using the cache version, uncomment the line below and comment the one after, vice versa.
 		# coin_to_exchanges[coin] = exchanges
-		# return exchanges
+		return exchanges
 
 	else:
 		print("Error updating " + coin + "!")
